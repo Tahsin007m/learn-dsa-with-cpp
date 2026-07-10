275a76d8b262d685235c73c9d6d399525fdc632e
Tahsin Reza <syedtahsinreza74789@gmail.com>
2026-07-10 21:12:04 +0600

    Add files via upload

---
+++ 01_operators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a = 10, b = 5;
+
+    cout << "sum is = " << a+b << endl;
+    cout << "difference is = " << a-b << endl;
+    cout << "product is = " << a*b << endl;
+    cout << "division is = " << a/b << endl;
+    cout << "modulus is = " << a%b << endl;
+
+    cout<<(5>3)<<endl; // true->1
+    cout<<(5<3)<<endl; // false->0
+    cout<<(5>=3)<<endl; // true->1
+    cout<<(0==0.1)<<endl; // true->0
+    
+    return 0;
+}
---
+++ 02_unary_operators.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num = 5;
+    num++; // a = a + 1
+
+ //   int a = 10;
+    
+  // cout<<"variable ++ method:"<<endl;
+  //  int b = a++; // execute first, then increment
+  //  cout<<"b: "<<b<<endl; // 10
+  //  cout<<"a: "<<a<<endl; // 11
+
+  //  cout<<"++variable method:"<<endl;
+
+  //  int c = 10;
+
+  //  int d = ++c; // increment first, then execute
+  //  cout<<"d: "<<d<<endl; // 12
+  //  cout<<"c: "<<c<<endl; // 12 
+
+    cout<<"variable -- method:"<<endl;
+
+    int a = 10;
+    int b = num1--;
+    cout<<"b: "<<b<<endl; // 9
+    cout<<"a: "<<a<<endl; // 10
+    
+    
+    return 0;
+}
